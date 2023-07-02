3db89f39aa58e55709605ea70a15609d8b6b37ff
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-07-02 21:58:56 +0700

    Order manager, order history (testing, not yet)

---
+++ BussinessObject/Models/Orders/OrderVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObject.Models
+{
+    public class OrderVM
+    {
+        public int? OrderId { get; set; }
+        public string MemberId { get; set; }
+        public string Username { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime ShipDate { get; set; }
+        public double Freight { get; set; }
+    }
+}
--- EStoreAPI/Controllers/OrderController.cs
+++ EStoreAPI/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DataAccess.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EStoreAPI.Controllers
@@ -7,5 +9,40 @@ namespace EStoreAPI.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var order = await _orderService.GetAllOrders();
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpGet("orderhistory/{userId}")]
+        public async Task<IActionResult> GetOrderHistory([FromRoute] string userId)
+        {
+            try
+            {
+                var result = await _orderService.GetOrdersHistoryByUser(userId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
     }
 }
