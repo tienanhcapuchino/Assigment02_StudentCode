756034253a7d0e13f53e49342d2a390be1dc4a5c
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-07-02 10:14:50 +0700

    update profile user role, add order mamagement page

--- BussinessObject/Models/User/UserProfileModel.cs
+++ BussinessObject/Models/User/UserProfileModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace BussinessObject.Models
 {
     public class UserProfileModel
     {
-        public string Id { get; set; }
+        public string? Id { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string? Username { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
     }
 }
---
+++ EStoreWeb/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using EStoreWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EStoreWeb.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ICommonService _commonService;
+        public OrderController(ICommonService commonService)
+        {
+            _commonService = commonService;
+        }
+        public IActionResult Index()
+        {
+            var tokenModel = _commonService.GetTokenData().GetAwaiter().GetResult();
+            if (tokenModel == null)
+            {
+                return Redirect("../Home/Index");
+            }
+            if (!tokenModel.RoleName.Equals("Admin"))
+            {
+                return Forbid();
+            }
+            return View();
+        }
+    }
+}
