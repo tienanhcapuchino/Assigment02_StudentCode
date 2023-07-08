89d56aca64ca4afca84d85637c55c86133b15f7c
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-07-08 10:23:20 +0700

    fix bug: sale report, header

--- EStoreWeb/wwwroot/js/saleReport.js
+++ EStoreWeb/wwwroot/js/saleReport.js
@@ -29,7 +29,7 @@ $('#createReport').click((e) => {
                 $("#sale-report").append(
                     response.map((item) => (
                         `<tr>
-                            <td>${item.orderDate}</td>
+                            <td>${formatDate(item.orderDate)}</td>
                             <td>${item.productName}</td>
                             <td>${item.unitPrice}$</td>
                             <td>${item.quantity}</td>
@@ -44,3 +44,11 @@ $('#createReport').click((e) => {
         },
     });
 });
+
+function formatDate(date) {
+    const d = new Date(date);
+    const day = d.getDate().toString().padStart(2, '0');
+    const month = (d.getMonth() + 1).toString().padStart(2, '0');
+    const year = d.getFullYear().toString();
+    return `${day}/${month}/${year}`;
+}
