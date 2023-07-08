7b386f874b09dc67c269762d5674caf8e9b5822c
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-07-08 10:01:10 +0700

    sale report

--- EStoreWeb/wwwroot/js/saleReport.js
+++ EStoreWeb/wwwroot/js/saleReport.js
@@ -1,3 +1,46 @@
-$('#createReport').click(() => {
+function getTokenFromCookie() {
+    const cookies = document.cookie.split("; ");
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split("=");
+        const name = cookie[0];
+        const value = cookie[1];
+        if (name === "token") {
+            return value;
+        }
+    }
+    return null;
+}
 
+var token = getTokenFromCookie();
+
+$('#createReport').click((e) => {
+    e.preventDefault();
+    var fromDate = $("#fromDate").val();
+    var toDate = $("#toDate").val();
+    $.ajax({
+        url: "http://localhost:5063/api/User/salereport/" + fromDate +"/" + toDate,
+        method: "GET",
+        contentType: "application/json",
+        headers: {
+            Authorization: "Bearer " + token,
+        },
+        success: (response) => {
+            $("#sale-report").empty()
+                $("#sale-report").append(
+                    response.map((item) => (
+                        `<tr>
+                            <td>${item.orderDate}</td>
+                            <td>${item.productName}</td>
+                            <td>${item.unitPrice}$</td>
+                            <td>${item.quantity}</td>
+                            <td>${item.sales}$</td>
+                        </tr>`
+                    ))
+                )
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            alert(xhr.responseText);
+        },
+    });
 });
